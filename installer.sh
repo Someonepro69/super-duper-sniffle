26fef8e8cf7bf5e47dd38f3a3f54dc4b134452d4
Caleb Noble <daarumadxxu@gmail.com>
2022-12-31 13:00:03 +0000

    install wheel

--- installer.sh
+++ installer.sh
@@ -41,7 +41,7 @@ clone_repo(){
 }
 
 install_requirements(){
-    pip install --upgrade pip
+    pip install --upgrade pip wheel
     echo -e "\n\nInstalling requirements... "
     pip3 install -q --no-cache-dir -r $DIR/requirements.txt
     pip3 install -q -r $DIR/resources/startup/optional-requirements.txt
